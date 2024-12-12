acd5e698a6cc5f6330509f75832c6b4a81783c89
Halleys123 <arnavadvik2003@gmail.com>
2024-12-12 23:30:57 +0530

    Implement theme initialization and refactor dark mode handling

--- src/frontend/src/utils/toggleDarkMode.js
+++ src/frontend/src/utils/toggleDarkMode.js
@@ -1,13 +1,8 @@
+import { initializeTheme } from "./init";
+
 export default function toggleDarkMode(darkMode, setDarkMode) {
   const newDarkMode = !darkMode;
   setDarkMode(newDarkMode);
-  if (newDarkMode) {
-    document.documentElement.classList.add("dark");
-    localStorage.setItem("darkMode", "true");
-    document.getElementById("root").style.backgroundColor = "#333";
-  } else {
-    document.documentElement.classList.remove("dark");
-    localStorage.setItem("darkMode", "false");
-    document.getElementById("root").style.backgroundColor = "#fff";
-  }
+  localStorage.setItem("darkMode", newDarkMode.toString());
+  initializeTheme(newDarkMode);
 }
