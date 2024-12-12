50ddf7e80ad1bbe64b0ca4795d05ebb50037d1d1
Halleys123 <arnavadvik2003@gmail.com>
2024-12-12 17:05:32 +0530

    Update SectionOne component layout and add new content sections

--- src/frontend/src/pages/Homepage/Sections/SectionOne.jsx
+++ src/frontend/src/pages/Homepage/Sections/SectionOne.jsx
@@ -1,12 +1,17 @@
 function SectionOne() {
   return (
     <section className="flex flex-col w-full h-screen">
-      <div className="flex flex-row justify-between items-center w-full">
-        <div className="flex flex-col gap-4">
+      <div className="flex flex-row justify-around items-center w-full flex-1">
+        <div className="flex flex-col gap-4 items-center">
           <span className="font-Exo font-medium text-4xl text-neutral-700">
             Welcome to the Developers Club
           </span>
         </div>
+      </div>
+      <div className="flex flex-row bg-neutral-200 h-64 p-1 gap-1">
+        <div className="flex flex-row gap-2 items-center justify-center flex-1 bg-white rounded-md h-full"></div>
+        <div className="flex flex-row gap-2 items-center justify-center flex-1 bg-white rounded-md h-full"></div>
+        <div className="flex flex-row gap-2 items-center justify-center flex-1 bg-white rounded-md h-full"></div>
       </div>
     </section>
   );
