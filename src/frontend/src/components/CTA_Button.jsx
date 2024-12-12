a037d0a26ea31e7b443a9d3522278f902420c7c1
Halleys123 <arnavadvik2003@gmail.com>
2024-12-12 23:14:36 +0530

    Added not found and somethingwebnt wrong page

--- src/frontend/src/components/CTA_Button.jsx
+++ src/frontend/src/components/CTA_Button.jsx
@@ -1,9 +1,18 @@
-export default function CTA_Button({ className }) {
+import PropTypes from "prop-types";
+
+CTA_Button.propTypes = {
+  className: PropTypes.string,
+  text: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default function CTA_Button({ className, text, onClick = () => {} }) {
   return (
     <button
+      onClick={onClick}
       className={`bg-black h-12 w-48 text-white font-Tektur tracking-wide rounded-full hover:bg-neutral-900 ${className}`}
     >
-      View All Events
+      {text || "View All Events"}
     </button>
   );
 }
---
+++ src/frontend/src/pages/NotFound.jsx
@@ -0,0 +1,43 @@
+import CTA_Button from "@/components/CTA_Button";
+import TextHighlighter from "@/components/Homepage/TextHighlighter";
+import MainLayout from "@/Layout/MainLayout";
+import { useNavigate } from "react-router-dom";
+
+function NotFound() {
+  const history = useNavigate();
+  return (
+    <MainLayout>
+      <section
+        className={`flex flex-col w-full min-h-screen dark:bg-neutral-900 bg-white`}
+      >
+        <div className="flex flex-col justify-center items-center flex-1 px-20 sm:px-24 md:px-48 lg:px-12">
+          <div className="flex flex-col gap-4 items-center">
+            <span
+              style={{ lineHeight: "1.5" }}
+              className={`font-Exo text-center font-medium text-2xl sm:text-3xl md:text-4xl dark:text-neutral-300 text-neutral-700`}
+            >
+              Oops!
+              <TextHighlighter
+                text="Page Not Found"
+                className="text-neutral-700 dark:text-neutral-800 bg-yellow-200 dark:bg-yellow-300"
+              />
+            </span>
+            <span className="font-Exo font-normal text-center text-md dark:text-neutral-300 text-neutral-600">
+              The page you are looking for might have been removed, had its name
+              changed, or is temporarily unavailable.
+            </span>
+            <CTA_Button
+              text="Go Back"
+              onClick={() => {
+                history(-1);
+              }}
+              className="mt-8"
+            />
+          </div>
+        </div>
+      </section>
+    </MainLayout>
+  );
+}
+
+export default NotFound;
