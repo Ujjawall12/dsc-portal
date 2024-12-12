81d31501fda4c20c983feeb61eaedf54341489d9
Halleys123 <arnavadvik2003@gmail.com>
2024-12-12 15:46:01 +0530

    Refactored NavbarButton.jsx component

--- src/frontend/src/components/NavbarButton.jsx
+++ src/frontend/src/components/NavbarButton.jsx
@@ -14,14 +14,22 @@ export default function NavbarButton({
 }) {
   const location = useLocation();
   const isActiveLink = location.pathname === to;
-  if (type == "default")
-    return (
-      <NavLink
-        to={to}
-        className={`flex flex-row gap-2 px-4 py-[2px] min-w-24 rounded-full outline outline-1 ${
-          isActiveLink ? "outline-neutral-800" : "outline-neutral-300"
-        } items-center justify-center hover:bg-neutral-100 transition-colors`}
-      >
+
+  const baseClasses =
+    "flex flex-row gap-2 px-4 py-1 min-w-24 rounded-full items-center justify-center transition-colors";
+  const defaultClasses = `outline outline-1 ${
+    isActiveLink ? "outline-neutral-800" : "outline-neutral-300"
+  } hover:bg-neutral-100`;
+  const blackClasses = "bg-black hover:bg-neutral-800";
+
+  return (
+    <NavLink
+      to={to}
+      className={`${baseClasses} ${
+        type === "default" ? defaultClasses : blackClasses
+      }`}
+    >
+      {type === "default" && (
         <div
           className={`rounded-full w-2 h-2 ${
             isActiveLink
@@ -29,28 +37,16 @@ export default function NavbarButton({
               : "bg-transparent outline outline-1 outline-neutral-400"
           }`}
         ></div>
-        <span
-          className={`h-[1.4rem] text-neutral-700 text-sm ${
-            isActiveLink && "font-semibold"
-          }`}
-        >
-          {text}
-        </span>
-      </NavLink>
-    );
-  else if (type == "black")
-    return (
-      <NavLink
-        to={to}
-        className="flex flex-row gap-2 px-4 py-1 min-w-24 rounded-full bg-black items-center justify-center hover:bg-neutral-800 transition-colors"
+      )}
+      <span
+        className={`${
+          type === "default" ? "text-neutral-700" : "text-white"
+        } text-sm ${isActiveLink && "font-semibold"} ${
+          type === "black" && "font-Exo tracking-widest"
+        }`}
       >
-        <span
-          className={`font-Exo tracking-widest text-white text-sm ${
-            isActiveLink && "font-semibold"
-          }`}
-        >
-          {text}
-        </span>
-      </NavLink>
-    );
+        {text}
+      </span>
+    </NavLink>
+  );
 }
