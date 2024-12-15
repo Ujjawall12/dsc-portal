5245a5e545d30e041fc71a0621cb94f0c2beb263
Arnav Chhabra <65032719+Halleys123@users.noreply.github.com>
2024-12-15 18:33:54 +0530

    Moved car dto a new component from section three

--- src/frontend/src/pages/Homepage/Sections/SectionThree.jsx
+++ src/frontend/src/pages/Homepage/Sections/SectionThree.jsx
@@ -1,75 +1,7 @@
 import image from 'assets/rasp_one.png';
 import cat from 'assets/cat.png';
-
 import PropTypes from 'prop-types';
-
-Card.propTypes = {
-  parentClassName: PropTypes.string.isRequired,
-  headingClassName: PropTypes.string,
-  innerDivClassName: PropTypes.string,
-  subheadingClassName: PropTypes.string,
-  textClassName: PropTypes.string,
-  imageClassName: PropTypes.string,
-  svgClassName: PropTypes.string,
-  contentType: PropTypes.string.isRequired,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  text: PropTypes.string,
-  link: PropTypes.string,
-  imageSrc: PropTypes.string,
-  imageAlt: PropTypes.string,
-  svgContent: PropTypes.element,
-};
-
-function Card({
-  parentClassName,
-  headingClassName,
-  innerDivClassName,
-  subheadingClassName,
-  textClassName,
-  imageClassName,
-  svgClassName,
-  contentType,
-  heading,
-  subheading,
-  text,
-  imageSrc,
-  link,
-  imageAlt,
-  svgContent,
-}) {
-  return (
-    <div className={parentClassName}>
-      {contentType === 'text' ? (
-        <>
-          <span className={headingClassName}>{heading}</span>
-          <div className={innerDivClassName}>
-            <span className={subheadingClassName}>{subheading}</span>
-            <span className={textClassName}>{text}</span>
-          </div>
-        </>
-      ) : contentType === 'image' ? (
-        <div className='flex items-center justify-center'>
-          <img src={imageSrc} alt={imageAlt} className={imageClassName} />
-        </div>
-      ) : (
-        <a
-          href={link}
-          target='_blank'
-          className='flex items-center justify-center'
-        >
-          <svg
-            className={svgClassName}
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 256 256'
-          >
-            {svgContent}
-          </svg>
-        </a>
-      )}
-    </div>
-  );
-}
+import Card from '../../../components/Homepage/Card'; // Import the Card component
 
 const cardData = [
   {
