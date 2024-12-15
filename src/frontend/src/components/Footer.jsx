a7a4e51011b2bcacf82540ffa1b7ceabdd1b9e9b
Arnav Chhabra <65032719+Halleys123@users.noreply.github.com>
2024-12-15 22:12:07 +0530

    Modifying secction four

--- src/frontend/src/components/Footer.jsx
+++ src/frontend/src/components/Footer.jsx
@@ -1,3 +1,16 @@
+import stars from 'assets/stars_two.png';
+
 export default function Footer() {
-  return <div className='flex flex-col'></div>;
+  return (
+    <div className='flex flex-col w-full'>
+      <div className='w-4/5 h-64 rounded-xl overflow-hidden relative bg-violet-300 mx-auto translate-y-16 footer-head'>
+        <img
+          src={stars}
+          alt='stars'
+          className='w-full mx-auto absolute animate-stars'
+        />
+      </div>
+      <div className='flex flex-row gap-4 min-h-72 bg-blue-200'></div>
+    </div>
+  );
 }
