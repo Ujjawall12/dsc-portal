fca7fc9ab3acb7e77b1234e41fa551a40b49e540
Arnav Chhabra <65032719+Halleys123@users.noreply.github.com>
2024-12-15 18:32:12 +0530

    Add SectionFour component with heading and description; update SectionThree layout

--- src/frontend/src/pages/Homepage/Sections/SectionFour.jsx
+++ src/frontend/src/pages/Homepage/Sections/SectionFour.jsx
@@ -1,3 +1,12 @@
-export default function SectionFour(params) {
-  return <></>;
+import SectionHeading from '@/components/SectionHeading';
+
+export default function SectionFour() {
+  return (
+    <div className='flex flex-col gap-4 min-h-screen px-8 py-12 mt-12'>
+      <SectionHeading
+        title='Expore Our Website'
+        description='Explore the different sections of our website.'
+      />
+    </div>
+  );
 }
