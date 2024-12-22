ee2c7de956d32e580cc55a9fb2220d34bd903c6c
Arnav Chhabra <65032719+Halleys123@users.noreply.github.com>
2024-12-22 17:12:25 +0530

    changed tem to teams

--- src/frontend/src/pages/Team.jsx
+++
@@ -1,7 +0,0 @@
-import MainLayout from "Layout/MainLayout";
-
-function Team() {
-  return <MainLayout></MainLayout>;
-}
-
-export default Team;
---
+++ src/frontend/src/pages/Teams.jsx
@@ -0,0 +1,7 @@
+import MainLayout from 'Layout/MainLayout';
+
+function Teams() {
+  return <MainLayout></MainLayout>;
+}
+
+export default Teams;
