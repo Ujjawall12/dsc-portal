53a379572f898d5278073e6e9c2f68368032233a
Arnav Chhabra <65032719+Halleys123@users.noreply.github.com>
2024-12-22 18:26:03 +0530

    Added some basic backend

---
+++ src/backend/schema/UserSchema.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const UserHistory = require('./UserHistorySchema');
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'member', 'participant'],
+      default: 'participant',
+    },
+    history: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'UserHistory',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Middleware to save history on update
+UserSchema.pre('findOneAndUpdate', async function (next) {
+  const update = this.getUpdate();
+  const user = await this.model.findOne(this.getQuery());
+  const history = new UserHistory({
+    userId: user._id,
+    updatedField: Object.keys(update)[0],
+    oldValue: user[Object.keys(update)[0]],
+    newValue: update[Object.keys(update)[0]],
+  });
+  await history.save();
+  next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
