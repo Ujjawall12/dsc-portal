73d33939ceedcfc915d92db2d0f0c7add6d6f103
Arnav Chhabra <65032719+Halleys123@users.noreply.github.com>
2025-02-02 23:54:26 +0530

    Minor changes in gitignore and removed some links

--- src/frontend/src/pages/Homepage/Homepage.jsx
+++ src/frontend/src/pages/Homepage/Homepage.jsx
@@ -1,14 +1,14 @@
-import MainLayout from "Layout/MainLayout";
-import SectionOne from "pages/Homepage/Sections/SectionOne";
-import SectionTwo from "./Sections/SectionTwo";
-import SectionThree from "./Sections/SectionThree";
-import SectionFour from "./Sections/SectionFour";
+import MainLayout from 'Layout/MainLayout';
+import SectionOne from 'pages/Homepage/Sections/SectionOne';
+import SectionTwo from './Sections/SectionTwo';
+import SectionThree from './Sections/SectionThree';
+import SectionFour from './Sections/SectionFour';
 
 function Homepage() {
   return (
     <MainLayout>
       <SectionOne />
-      <SectionTwo />
+      {/* <SectionTwo /> */}
       <SectionThree />
       <SectionFour />
     </MainLayout>
